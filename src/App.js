2d6c114c93056828486e8cd6b5291759ad62a5b4
Sarah P <poultonse@gmail.com>
2021-08-12 11:37:36 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import { Link, Route} from 'react-router-dom';
+import topics from './topics.js';
+
+function Home(){
+    return <h1>HOME</h1>
+}
+
+function Topics(){
+    return (
+        <div>
+            <h1>TOPICS</h1>
+            <ul>
+                {topics.map(({ name, id})=> (
+                    <li key={id}>
+                        <Link to={`/topics/${id}`}>{name}</Link>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Some words here</h2>
       </header>
+
+        <div>
+            <ul >
+                <li><Link to='/'>HOME</Link></li>
+                <li><Link to='topics'>TOPICS</Link></li>
+            </ul>
+
+            <Route exact path='/'>
+                <Home />
+            </Route>
+
+            <Route path='/topics'>
+                <Topics />
+            </Route>
+
+        </div>
+
     </div>
   );
 }
